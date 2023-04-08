88c87b7a5ad55258e2e8b9fef4566e7cfb927567
diariesofijon <86806697+diariesofijon@users.noreply.github.com>
2023-04-08 03:15:38 +0300

    refactoring output of the graph list in the console

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
-import re
 from pprint import pprint
-from . import graph_engine
+import graph_engine
 
 
 def main():
     ''' Converting list of links to a graph '''
     print('input the graph')
     tmp = input()
-    srg = graph_engine,.StringRepresentationGraph(tmp=tmp).to_pythonic()
-
+    print('\n\n\n')
+    srg = graph_engine.StringRepresentationGraph(tmp=tmp).to_pythonic()
     print('\n\n\n')
     pprint(repr(srg))
 
