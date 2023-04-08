93a1b319394a15d072ee6ee82532e0ec7e75dcbf
diariesofijon <86806697+diariesofijon@users.noreply.github.com>
2023-04-08 04:43:38 +0300

    last input funcationality

--- main.py
+++ main.py
@@ -2,16 +2,23 @@
 
 from pprint import pprint
 import graph_engine
+import sys
 
 
 def main():
     ''' Converting list of links to a graph '''
     print('input the graph')
-    tmp = input()
+    if sys.argv[0] == '-from_console':
+        tmp = input()
+    else:
+        with open('graph_links.txt') as file:
+            tmp = file.read()
     print('\n\n\n')
-    srg = graph_engine.StringRepresentationGraph(tmp=tmp).to_pythonic()
+    srg = graph_engine.StringRepresentationGraph(tmp=tmp)
     print('\n\n\n')
-    pprint(repr(srg))
+    print(str(srg))
+    with open('output.txt', 'w') as file:
+        file.write(str(srg))
 
 if __name__ == '__main__':
     main()
