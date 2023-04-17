aaa79a1f2189a903c8236b8cf5d127d068207ed6
diariesofijon <86806697+diariesofijon@users.noreply.github.com>
2023-04-17 04:16:32 +0300

    refactoring graph_engine.py due logic migration to config.py

--- graph_engine.py
+++ graph_engine.py
@@ -5,19 +5,14 @@ from xml.etree import ElementTree
 from dataclasses import dataclass
 from typing import Optional
 
+import config
+
 
 __all__ = ('RepresentativeGraphElement', 'StringRepresentationGraph')
 
 
 
-@dataclass
-class StringRepresentationGraph:
-
-    element_mask: str = r'.+(?P<id>\D+)\..?(?P<grouped>.+): (?P<body>.*)\n'
-    node_mask: str = r'(?P<id>\D+)\((?P<children_list>.*)\)'
-    part_mask: str = r'.*(?P<id>\S+\D+\).\n'
-    tmp: str = ''
-    separeter: str = '\n'
+class StringRepresentationGraph(config.StringByStringRegularExpressionMask):
 
     def __iter__(self):
         return iter(self._get_formated_links())
@@ -38,59 +33,14 @@ class StringRepresentationGraph:
     def get_element(self, part=None, id=None) -> RepresentativeGraphElement:
         return self.get_elements(part, id)[0]
 
-    def _get_formated_links(self):
-        last_part = 'A1.'
-        
-        for link in self.tmp.split('\n'):
-            if (tmp := link.strip()).endswith('.'):
-                last_part = tmp
-                continue
-            yield from self._convert_element(tmp, last_part)
-    
-    @staticmethod
-    def _get_ids(name):
-            if len(tmp := name.split('-')) == 2:
-                return list(range(int(tmp[0]), int(tmp[1])+1))
-            return [int(name)]
-
-    @staticmethod
-    def _convert_element(tmp, last_part):
-        ids, tmp = tmp.split('.')
-        for id in StringRepresentationGraph._get_ids(ids):
-            grouped, body = tmp.split(':')
-            data = RepresentativeGraphElement(
-                id=id, grouped=grouped, part=last_part, body=body)
-            yield data
-
 
 @dataclass
-class RepresentativeGraphElement:
+class RepresentativeGraphElement(config.RepresentativeGraphElementAbstract):
     
-    id: str = ''
-    part: str = ''
-    grouped: str = ''
-    body: str = ''
     graph: Optional[StringRepresentationGraph] = None
     
     def __str__(self):
         return f'{self.part} id: {self.id} = {self.grouped} - {self.body}'
-    
-    def children(self):
-        if self.graph:
-            return list(*self.graph.get_elements(
-                part=self.part, id=self.id))
-        return []
-        
-    
-    @classmethod
-    def load(cls, file=None, string=None, part=None, id=None):
-        if file:
-            with open(file, 'r') as file:
-                return StringRepresentationGraph(tmp=file).get_element(part, id)
-        elif string:
-            return StringRepresentationGraph(tmp=string).get_element(part, id)
-        else:
-            raise Indexerror('Unexpected behavior')
 
 
 class XmlGraphElementMixin(ElementTree.Element):
