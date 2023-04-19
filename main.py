2bdf4be1f11d4c889e36cfac107e402d704525a8
diariesofijon <86806697+diariesofijon@users.noreply.github.com>
2023-04-19 16:37:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ def main():
     ''' Converting list of links to a graph '''
     print('input the graph')
     print('\n\n\n')
-    if sys.argv[0] and sys.argv[0] == '-from_console':
-        srg = graph_engine.StringRepresentationGraph(tmp=(tmp := input()))
-    elif (file := sys.argv[1]) and sys.argv[1].endswith('.txt'):
-        srg = graph_engine.StringRepresentationGraph(file))
+    if len(sys.argv) and sys.argv[0] == '-from_console':
+        print(1)
+        srg = graph_engine.StringByStringRegularExpressionMask(tmp=input())
+    elif (len(sys.argv)-1) and (file := sys.argv[1]) and sys.argv[1].endswith('.txt'):
+        print(2)
+        srg = graph_engine.StringByStringRegularExpressionMask(file=file)
     else:
-        srg = graph_engine.StringRepresentationGraph()
+        print(3)
+        srg = graph_engine.StringByStringRegularExpressionMask()
     print('\n\n\n')
     print(str(srg))
     with open('output.txt', 'w') as file:
