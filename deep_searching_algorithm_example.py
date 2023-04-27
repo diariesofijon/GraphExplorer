56a5018e9319ebc184260435afa70f7c0e6c0932
diariesofijon <86806697+diariesofijon@users.noreply.github.com>
2023-04-27 04:09:37 +0300

    Undo syntax refactoring

--- deep_searching_algorithm_example.py
+++ deep_searching_algorithm_example.py
@@ -10,12 +10,9 @@ from xml.etree import ElementTree
 d = {'red': 0, 'green': 0, 'blue': 0}
 
 def getchildren(root, level=1):
-    if root.attrib["color"] == 'red':
-        d['red'] += level
-    elif root.attrib['color'] == 'green':
-        d['green'] += level
-    else:
-        d['blue'] += level
+    if root.attrib["color"] == 'red': d['red'] += level
+    elif root.attrib['color'] == 'green': d['green'] += level
+    else: d['blue'] += level
     level += 1
     for element in root: getchildren(element, level)
 
