3996bd28e6971630b1304ecdbb4b311927c7701e
diariesofijon <86806697+diariesofijon@users.noreply.github.com>
2023-05-05 02:14:52 +0300

    refactoring and adding docstring of confing.py

--- confing.py
+++ confing.py
@@ -3,6 +3,13 @@
 # pyling: disable=W0622
 # pyling: disable=E0602
 
+'''
+        This file contains all settings of the project like constans, 
+    dictionaries, list and variables. All of data are strictly designed due idea
+    of keeping easear to change behavior of anythings deal of the module in the
+    runtime.
+'''
+
 SEPARATES = {
 	'NODE': '\n'
 }
