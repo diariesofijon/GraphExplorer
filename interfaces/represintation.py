3c03564dca260de62730600b4e6ad6eace618ccf
Nikita Osipov <osipov.engineer@gmail.com>
2024-12-19 06:37:13 +0300

    first scratch of polymorphic represintation

---
+++ interfaces/represintation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+'''
+	There are pull of scripts contains representation of any varitant of
+data visualisation wich can be affordably used in any useful place in the code.
+'''
+
+import sys
+import os
+
+from bin import metaclasses, protocols
+from lib import shortcuts, typing
+
+# TODO: declare protocol to exclude useful info from origin source data
+# TODO: RegExp Conception future planning
