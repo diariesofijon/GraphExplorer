4001baf07896347699a64bd7c2376d40f35c76f1
Nikita Osipov <osipov.engineer@gmail.com>
2024-12-28 11:34:55 +0300

    basic cut protocol  to indentify where is algortighm could be work

--- bin/protocols.py
+++ bin/protocols.py
@@ -46,3 +46,74 @@ class VertexProtocol(GraphInfoProtocol):
 
     def choose_graph(self, depth: int):
         ...
+
+
+class GraphsDunderMethodsMixinProtocol(Protocol):
+
+    def __iter__(self) -> typing.GGE:
+        ...
+
+    def __len__(self) -> int:
+        ...
+
+    def __str__(self) -> str:
+        ...
+
+    def __repr__(self) -> str:
+        ...
+
+    def __getitem__(self, key: int) -> typing.GE:
+        ...
+
+    def __contains__(self, element: typing.GE) -> bool:
+        ...
+
+    def __del__(self):
+        ...
+
+
+@runtime_checkable
+def GraphProtocol(Protocol):
+
+    def is_bipartite(self) -> bool:
+        ...
+
+    def loader(self) -> typing.Loader:
+        ...
+
+    def dfs(self, vertex: int) -> Tuple[List]:
+        ...
+
+    def bfs(self, node: typing.GE, visited: List) -> Tuple[List]:
+        ...
+
+
+@runtime_checkable
+class TreeProtocol(Protocol):
+
+    def graph(self) -> GraphProtocol:
+        ...
+
+    def element_ids(self) -> List[int]:
+        ...
+
+    def top(self) -> typing.GE:
+        ...
+
+    def longest_chain(self) -> Iterable:
+        ...
+
+    def depth(self) -> int:
+        ...
+
+    def dfs(self) -> Tuple[List]:
+        '''
+        Best case performance for a depth-first algorithm is O(1),
+        while worst case performance is O(N). In another words The time
+        complexity of the DFS algorithm is O(V+E), where V is the number
+        of vertices and E is the number of edges in the graph.
+        '''
+        ...
+
+    def bfs(self, node: typing.GE, visited: List) -> Tuple[List]:
+        ...
