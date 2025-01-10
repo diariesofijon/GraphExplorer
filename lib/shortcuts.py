f4405e78f001fd23f50aaa8ad18639a592de0cbc
Nikita Oispov <osipov.engineer@gmail.com>
2025-01-10 14:30:39 +0300

    changes

--- lib/shortcuts.py
+++ lib/shortcuts.py
@@ -12,6 +12,8 @@ def get_ids(name: str, separeter: str='-') -> List[int]:
     ''' Clear function that implement name to ids '''
     if len((tmp := name.split(separeter))) == 2:
         return list(range(int(tmp[0]), int(tmp[1])+1))
+    elif not name:
+        return []
     return [int(name)]
 
 def eisenhower_part_spliter(tmp: str) -> Tuple[str]:
