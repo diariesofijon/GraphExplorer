000cf805381f27885ee2e71da52840a9ca364083
Osipov Nikita Alexandrovich <86806697+diariesofijon@users.noreply.github.com>
2025-10-04 05:43:08 +0300

    resolving "TODO: reimagine conception to undo commented strings" FIRST REALIZATION

--- lib/chains.py
+++ lib/chains.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python
 # pylint: disable=E0401
 
-from typing import Iterable, Dict, Tuple, Callable
+from typing import Iterable, Dict, Tuple, Callable, Optional, List
 
 from bin import metaclasses
 from lib import abc, shortcuts, typing
 import config
 
 
+Callback = Optional[Callable]
+
+
 class BaseChain(abc.AbstractChain):
 
-    # TODO: reimagine conception to undo commented strings
-    # blank: bool = False
+    '''
+        Determines the base principles in the relationship between data 
+    structures in this module. It is realized as an API for interacting with
+    any other pythonic data from the module and is supposed to use it as
+    pythonic list or just a collection if it could be needed!
+    '''
+
     blank: bool = True
 
-    def __init__(self, data: Optional[Iterable[T]] = None, unique: bool = True) -> None:
-        # TODO: reimagine conception to undo commented strings
-        # if self.blank:
-        #     flambda: Callable[Iterable] = self.skip_blank
-        # else:
-        #     flambda: Callable[Iterable] = self.store_blank
-        flambda = self.store_blank
+    def __init__(self, 
+        data: Optional[Iterable[T]]= None, unique:bool= True, flambda:Callback = None):
+        flambda: Callable = self.flambda if not flambda else flambda
         self._data: List[T] = list(data) if data else []
         if unique:
             # ensure uniqueness while preserving order
             seen = set()
-            self._data = [x for x in self._data if not (x in seen or seen.add(x))]
-        
-        super().__init__(filter(flambda, iterable), *args, **kwargs)
+            self._data = (
+                x for x in self._data if not (x in seen or seen.add(x)))
+        super().__init__(filter(self.flambda, iterable), *args, **kwargs)
 
     # --- Sequence API ---
     def __getitem__(self, index: int) -> T:
@@ -36,7 +40,7 @@ class BaseChain(abc.AbstractChain):
     def __len__(self) -> int:
         return len(self._data)
 
-    def __iter__(self) -> Iterator[T]:
+    def __iter__(self) -> Iterator[abc.E]:
         return iter(self._data)
 
     def __contains__(self, item: object) -> bool:
@@ -60,19 +64,19 @@ class BaseChain(abc.AbstractChain):
         # memory footprint of chain
         return super().__sizeof__() + sum(sys.getsizeof(x) for x in self._data)
 
-    def __copy__(self) -> "AbstractChain[T]":
+    def __copy__(self) -> typing.Chain[abc.E]:
         return self.__class__(self._data)
 
-    def __deepcopy__(self, memo) -> "AbstractChain[T]":
+    def __deepcopy__(self, memo) -> typing.Chain[abc.E]:
         import copy
         return self.__class__(copy.deepcopy(self._data, memo))
 
     # --- High-level Utilities ---
-    def start(self, index: int = 0) -> T:
+    def start(self, index: int = 0) -> abc.E:
         """First element from index (default first element)."""
         return self._data[index]
 
-    def end(self, index: int = 1) -> T:
+    def end(self, index: int = 1) -> adc.E:
         """Last element from offset (default last element)."""
         return self._data[-index]
 
@@ -93,19 +97,14 @@ class BaseChain(abc.AbstractChain):
 
 class TxtChain(BaseChain):
 
+    '''
+        Works only with TxtLoader or simple text from RAM!
+    '''
+
     __metaclass__ = metaclasses.MetaChain
 
-    # TODO: reimagine conception to undo commented strings
-    # blank: bool = False
     blank: bool = True
 
-    # TODO: reimagine conception to undo commented strings
-    # TODO: add any logger to store whole collision
-    # def store_blank(self, element) -> bool:
-    #     raise ValueError('Unexpected data')
-
-    # TODO: reimagine conception to undo commented strings
-    # temporary code
     def store_blank(self, element) -> bool:
         part, indicator = shortcuts.eisenhower_part_spliter(element)
         self.whole_parts[str(part)] += self.increase_on
@@ -115,24 +114,23 @@ class TxtChain(BaseChain):
 
 class GraphChain(TxtChain):
 
-    _cache = None
+    _cache: Optional[Tuple] = None
 
     @property
     def cached_index_tuple(self) -> tuple:
         '''Should use as interable chain of indexes of children'''
-        if self._cache:
-            return self._cache
+        if self.cache:
+            return self.cache
 
-        last_chain, visited = [], self.deepest_chain
-        for index, element in enumerate(visited):
-            if index == (len(visited) - 1):
-                break
+        last_chain: List[abc.E] = []
+        visited: typing.Chain   = self.deepest_chain
+        for index, element in visited.get_seed()):
             try:
                 last_chain += element.children.idex(index+1)
             except ValueError:
                 raise config.ThatIsNotGraph()
-        self._cache = tuple(*last_chain)
-        return self._cache
+        self.cache = tuple(*last_chain)
+        return self.cache
 
     @property
     def deepest_chain(self) -> TxtChain:
@@ -147,18 +145,18 @@ class GraphChain(TxtChain):
 
 class EisenhowerMatrixChain(GraphChain):
 
+    '''
+        Also works like text's driver but stores data about a perfectly done
+    filtering process for structuring the Eisenhower Matrix from the Graph.
+    Firstly, and as conception works as part of that engine.
+    '''
+
     blank: bool       = True
     whole_parts: Dict = {'A1.': 0, 'B2.': 0, 'C3.': 0, 'L4.': 0}
     increase_on: int  = 1
 
     def skip_blank(self, element) -> bool:
         return not self.blank
-
-    # TODO: reimagine conception to undo commented strings
-    # def store_blank(self, element) -> bool:
-    #     part, indicator = shortcuts.eisenhower_part_spliter(element)
-    #     self.whole_parts[str(part)] += self.increase_on
-    #     return bool(indicator)
 
 class JSONChain(GraphChain):
     pass
